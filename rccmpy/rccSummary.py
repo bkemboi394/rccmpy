c4ed7ccb0a3866641bac000f71ff96f81d6c8e6b
bkemboi394 <kemboib@mail.gvsu.edu>
2023-04-07 03:00:30 -0700

    renamed folder

---
+++ rccmpy/rccSummary.py
@@ -0,0 +1,266 @@
+# -*- coding: utf-8 -*-
+
+
+import numpy as np
+import math
+from scipy import stats
+from rccSim import rccSim
+from rccm import rccm
+
+
+# p=10
+# x = np.random.random((p,p))
+# M = np.random.random((p,p))
+# nu=20
+def dwishart (x, M, nu, logged = True):
+    
+  
+    """
+    Probability Density Function for Wishart Distribution:
+    
+    This function provides the probability density function for the Wishart distribution.
+    
+    Parameters
+    ----------
+    x : numpy.ndarray
+        p x p positive definite matrix.
+    M : numpy.ndarray
+        p x p mean matrix. Note that M = nu * V where V is the scale matrix.
+    nu : float
+        Degrees of freedom.
+    logged : bool, optional
+        If True, probability given on log scale.
+    
+    Returns
+    -------
+    float
+        The probability density function value.
+    
+    """
+    x = (x + np.transpose(x)) / 2
+    M = (M + np.transpose(M)) / 2
+    p = x.shape[0]
+    lnumr = (nu - p - 1) /( 2 * np.log(np.linalg.det(x))) - (nu / (2 * np.sum(np.diag(np. linalg. inv(x) * x))))  
+    ldenom = (nu * p / 2) * np.log(2) + (nu / 2) * np.log(np.linalg.det(1 / nu * M)) + (p * (p - 1) / 4) * \
+                                   np.log(np.pi) + np.sum([math.lgamma(nu / 2 + (1 - j) / 2) for j in range(1, p+1)])
+    return (lnumr - ldenom) if logged else (np.exp(lnumr - ldenom))
+    
+              
+def adj(mat, thresh = 0.001):
+    """
+    Adjacency Matrix:
+    
+    This function calculates an adjacency matrix for the matrix mat based on the absolute value threshold of thresh.
+    
+    Parameters
+    ----------
+    mat : numpy.ndarray
+        Numeric matrix.
+    thresh : float, optional
+        Threshold for absolute value of entries.
+    
+    Returns
+    -------
+    numpy.ndarray
+        An adjacency matrix containing 1's and 0's.
+    
+    """
+    return((abs(mat) > thresh) + 0)
+
+
+def zToA(z):
+    """
+    Z to Adjacency Matrix:
+    
+    This function calculates an adjacency matrix based on an integer vector of cluster memberships.
+    
+    Parameters
+    ----------
+    z : numpy.ndarray
+        Integer vector of cluster memberships.
+    
+    Returns
+    -------
+    numpy.ndarray
+        An adjacency matrix containing 1's and 0's.
+    
+    Examples
+    --------
+    # Calculate adjacency matrix for clustering
+    zToA(np.concatenate([np.repeat(1, 2), np.repeat(2, 2), np.repeat(3, 2)]))
+    
+    """
+    K = len(z)
+    A = np.zeros((K,K))
+    for r in range(K):
+        for s in range(K):
+           if z[r] != 0 and z[s] != 0:
+               A[r, s] = int(z[r] == z[s])
+           else:
+               A[r, s] = 0
+    return A
+
+
+
+
+              
+def randCalc(x,y):
+    """
+   Rand Index:
+   
+   This function calculates the rand index describing the amount of agreement between two integer vectors of cluster memberships.
+   
+   Parameters
+   ----------
+   x : numpy.ndarray
+       First integer vector of cluster memberships.
+   y : numpy.ndarray
+       Second integer vector of cluster memberships.
+   
+   Returns
+   -------
+   float
+       The rand index value, bounded between 0 and 1.
+   
+   """
+        
+    Ahat = zToA(x)[np.tril_indices_from(zToA(x ), k= -1)]
+    A0 = zToA(y)[np.tril_indices_from(zToA(y), k= -1)]
+    return((sum((Ahat - A0) == 2) + sum((Ahat - A0) == 0)) / math.comb(len(x), 2))
+        
+       
+
+
+
+def rccmLogLike(omegaks,omega0s,x,ws,lambda2):
+    """
+   Model Log-Likelihood:
+       
+   This function calculates the model log-likelihood
+   for the random covariance clustering model (RCCM)
+
+   Parameters
+   ----------
+   omegaks: np.ndarray
+       K x p x p array of K number of estimated subject-level precision matrices.
+   omega0s: np.ndarray
+       nclusts x p x p array of nclusts number of estimated cluster-level precision matrices.
+   ws: np.ndarray
+       nclusts x K matrix of estimated cluster weights for each subject (weights).
+   x: list
+       List of K data matrices each of dimension n_k x p.
+   lambda2: float
+       Non-negative scalar value used as input to rccm function to obtain estimates.
+
+   Returns
+   -------
+   float: Model log-likelihood
+
+   Examples
+   --------
+   # Generate data
+   np.random.seed(1994)
+   myData = rccSim(G=2, clustSize=10, p=10, n=100, overlap=0.50, rho=0.10)
+
+   # Analyze with RCCM
+   resultRccm = rccm(x=myData['simDat'], lambda1=20,
+                     lambda2=325, lambda3=0.01, nclusts=2)
+
+   # Calculate model log-likelihood
+   rccmLogLike(omegaks=resultRccm['Omegas'], omega0s=resultRccm['Omega0'],
+               ws=resultRccm['weights'], x=myData['simDat'], lambda2=325)
+   """
+
+    G = len(omega0s)
+    K = len(omegaks)
+
+    mll = 0
+    for k in range(K):
+        lk1 = np.sum(stats.multivariate_normal.logpdf(x[k],mean = np.mean(x[k], axis =0) ,\
+                                                      cov = np.linalg.inv(omegaks[k])))
+        # lk1 = np.sum(stats.multivariate_normal.logpdf(x[k], mean=np.zeros(len(omegaks[k])), \
+        #              cov = np.linalg.inv(omegaks[k])))
+        if any(ws[k,:] == 1):
+          lk2 = dwishart(omegaks[k], M = omega0s[np.where(ws[k,:] == 1)[0][0]],logged = False, nu = lambda2)
+          mll+= lk1 + lk2
+        else:
+           list_g = [g for g in range(G)]
+           lk2 = np.log(sum(list(map(lambda g: ws[k,g]*dwishart(omegaks[k], \
+                                                 M = omega0s[g],logged = False, nu = lambda2),list_g))))
+               
+           # lk2 = np.log(np.sum(np.array([ws[k,g]*dwishart(omegaks[k], nu=lambda2,\
+           #                                      M = omega0s[g], logged=False) for g in range(G)])))
+           mll+= lk1 + lk2  
+    return mll
+     
+    
+
+
+def aic(omegaks, omega0s, ws, x, lambda2):
+    """
+   AIC:
+   
+   This function calculates the AIC value for the random covariance clustering model (RCCM).
+   
+   Parameters:
+   omegaks (np.ndarray): K x p x p array of K number of estimated subject-level precision matrices.
+   omega0s (np.ndarray): nclusts x p x p array of nclusts number of estimated cluster-level precision matrices.
+   ws (np.ndarray): nclusts x K matrix of estimated cluster weights for each subject (weights).
+   x (list): List of K data matrices each of dimension n_k x p.
+   lambda2 (float): Non-negative scalar value used as input to rccm function to obtain estimates.
+
+   Returns:
+   float: Numeric AIC value.
+   
+   Examples:
+       
+    # Generate data
+    set.seed(1994)
+    myData = rccSim(G = 2, clustSize = 10, p = 10, n = 100, overlap = 0.50, rho = 0.10)
+    
+    # Analyze with RCCM
+    resultRccm = rccm(x = myData['simDat'], lambda1 = 20,
+                   lambda2 = 325, lambda3 = 0.01, nclusts = 2)
+
+   # Calculate AIC
+   aic(omegaks = resultRccm['Omegas'], omega0s = resultRccm['Omega0'],
+          ws = resultRccm['weights'], x = myData['simDat'], lambda2 = 325)
+   
+   """
+    K = len(omegaks)
+    G = len(omega0s)
+    
+    X =[k for k in range(K)]
+    dfks = map(lambda k: sum(adj(omegaks[k][np.tril_indices_from(omegaks[k])])),X)
+    
+    list_G = [g for g in range(G)]
+    dfgs = map(lambda g: sum(adj(omega0s[g][np.tril_indices_from(omega0s[g])])),list_G)
+    
+    
+    modelDim = sum(list(dfks), list(dfgs))
+    mll = rccmLogLike(omegaks = omegaks, omega0s = omega0s, ws = ws, x = x, lambda2 = lambda2)
+
+    aic = 2*modelDim - 2*mll
+    return(aic)
+
+    
+    
+    
+  # Generate data
+
+myData = rccSim(G = 2, clustSize = (10,12), p = 10, n = 100, overlap = 0.50, rho = 0.10, esd=0.05, graphtype='hub', eprob=0.5)
+ 
+  # Analyze with RCCM
+resultRccm = rccm(x = myData['simDat'], lambda1 = 20,
+                lambda2 = 325, lambda3 = 0.01, nclusts = 2)
+
+# Calculate AIC
+A = aic(omegaks = resultRccm['Omegas'], omega0s = resultRccm['Omega0'],
+        ws = resultRccm['weights'], x = myData['simDat'], lambda2 = 325)
+
+print(A)
+    
+    
+    
+    
+    
---
+++ rccmpy/rccm.py
@@ -0,0 +1,232 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+Random Covariance Clustering Model
+
+This function implements the Random Covariance Clustering Model (RCCM) for joint estimation of
+sparse precision matrices belonging to multiple clusters or groups. 
+
+Optimization is conducted using block coordinate descent.
+
+Parameters:
+x : list
+List of K data matrices each of dimension n_k x p
+lambda1 : float
+Non-negative scalar. Induces sparsity in subject-level matrices.
+lambda2 : float
+Non-negative scalar. Induces similarity between subject-level matrices and cluster-level matrices.
+lambda3 : float
+Non-negative scalar. Induces sparsity in cluster-level matrices.
+nclusts : int
+Number of clusters or groups.
+delta : float
+Threshold for convergence.
+max.iters : int
+Maximum number of iterations for block coordinate descent optimization.
+z0s : list
+Vector of length K with initial cluster memberships.
+
+Returns:
+result : dict
+A dictionary containing:
+- Omega0: nclusts x p x p array of nclusts number of estimated cluster-level precision matrices.
+- Omegas: K x p x p array of K number of estimated subject-level precision matrices.
+- weights: nclusts x K matrix of estimated cluster weights for each subject.
+
+Examples:
+Generate data with 2 clusters with 12 and 10 subjects respectively,15 variables for each subject, 
+100 observations for each variable for each subject,the groups sharing about 50% of network connections, 
+and 10% of differential connections within each group.
+
+myData = rccSim(G=2, clustSize=[12, 10], p=15, n=100, overlap=0.50, rho=0.10)
+
+Analyze simulated data with RCCM
+result = rccm(x=myData['simDat'], lambda1=10, lambda2=50, lambda3=2, nclusts=2, delta=0.001)
+"""
+
+
+import numpy as np
+import sklearn
+from sklearn.covariance import GraphicalLasso
+from scipy.cluster.hierarchy import linkage, fcluster
+from scipy.spatial.distance import squareform
+
+def rccm(x,  nclusts, lambda1, lambda2, lambda3=0, delta=0.001, max_iters=100, z0s=None, ncores=1):
+        
+   # Function for making almost symmetric matrix symmetric
+    def make_symmetric(x):
+        return (x + x.T) / 2
+    #Inputs
+    K = len(x)
+    G = nclusts
+    p = x[0].shape[1]
+    Sl = [np.cov(i, rowvar=False) for i in x]
+    nks = [i.shape[0] for i in x]
+    
+    # Initializing subject-level matrices
+    Omegas = []
+    for k in range(K):
+        pdStart = Sl[k] + np.eye(p) * 1e-6
+        gl = GraphicalLasso(alpha=0.001, mode='cd', tol=1e-4, verbose=False, 
+                            enet_tol=1e-4, max_iter=100) #warm_start=True)
+        gl.fit(pdStart)
+        Omegas.append(make_symmetric(gl.precision_))
+        
+    
+    
+    # Initializing weights using hierarchical clustering based on dissimilarity matrix of
+     # Frobenius norm of glasso matrix differences
+
+    K = len(Omegas)
+    distMat = np.empty((K, K))
+    for r in range(K):
+        for s in range(K):
+            distMat[r, s] = np.linalg.norm(Omegas[r,:,:] - Omegas[s,:,:], ord='fro')
+    
+    
+    
+
+    if z0s is None:
+        linkage_matrix = linkage(squareform(distMat), method='ward')
+        cl0 = fcluster(linkage_matrix, G, criterion='maxclust')
+    else:
+        cl0 = z0s
+        
+    wgk = np.empty((G, K))
+    for i in range(G):
+        for j in range(K):
+            wgk[i, j] = 1 if cl0[j] == (i + 1) else 0
+            
+    # Initializing cluster-level matrices to be all 0's
+    Omega0 = np.zeros((G,p, p))
+    Omegas_old = np.zeros((K,p, p))
+    Omega0_old = np.zeros((G,p, p))
+    counter = 0
+    
+    # Initializing vector for deltas and array for weights across iterations
+    deltas = []
+    wArray = np.full((max_iters + 1, G, K), np.nan)
+    wArray[0,:, :] = wgk
+    
+    
+    
+    
+    
+    
+    #Start BCD Algorithm
+    while max(abs(Omega0 - Omega0.old)) > delta or max(abs(Omegas - Omegas.old)) > delta or counter < 1:
+        counter += 1
+        
+        # Exit if exceeds max.iters
+        if counter >= max_iters:
+            res = {"Omega0": Omega0, "Omegas": Omegas, "weights": wgk}
+            print(f"Omegas fail to converge for lambda1 = {lambda1}, lambda2 = {lambda2}, lambda3 = {lambda3}, delta = {deltas[counter - 1]}")
+            # Returning results
+            return res
+        
+        # record current Omega0 & Omegas
+        Omega0_old = Omega0
+        Omegas_old = Omegas
+        
+        # 1st step: Updating pi's
+        pigs = np.sum(wgk, axis=1) / K
+        
+        # 2nd step: updating cluster-level precision matrices
+
+         # Calculating weighted-sum of subject-level matrices
+        
+        # Calculating weighted-sum of subject-level matrices
+        inv0 = np.zeros((G,p, p))
+        s0 = np.zeros((G,p, p))
+        
+        for g in range(G):
+            s0[g, :, :] = np.sum([wgk[g,k] * Omegas[k,:,:] for k in range(K)], axis=0) / np.sum(wgk[g,:])
+            penMat = np.full((p, p), lambda3 / (lambda2 * np.sum(wgk[g,:])))
+            np.fill_diagonal(penMat, 0)
+            if counter > 1:
+                L = np.linalg.cholesky(s0[g, :, :])
+                s0_inv = np.linalg.solve(L.T, np.linalg.solve(L, np.eye(p)))
+                Omega0[g, :, :] = s0_inv - np.diag(np.diag(s0_inv)) + penMat / lambda2
+            else:
+                Omega0[g, :, :] = np.linalg.inv(s0[g, :, :])
+            inv0[g, :, :] = np.linalg.inv(Omega0[g, :, :])
+
+        # inv0 = np.zeros((G,p, p))
+        
+        # s0 = np.array([np.sum([wgk[g,k] * Omegas[k,:, :] for k in range(K)], axis=0) for g in range(G)])
+        
+        
+        # for g in range(G):
+        #             S0 = s0[g,:, :] / np.sum(wgk[g, :])
+        #             penMat = np.full((p, p), lambda3 / (lambda2 * np.sum(wgk[g, :])))
+        #             np.fill_diagonal(penMat, 0)
+        #             if counter > 1:
+        #                 Omega0[g,:, :] = spcov.spcov(Sigma=Omega0[g,:, :], S=S0, lambd=penMat,\
+        #                                               tol_outer=delta, step_size=100)['Sigma']
+        #             else:
+        #                 Omega0[g,:, :] = spcov.spcov(Sigma=np.linalg.inv(S0), S=S0, lambd=penMat,\
+        #                                               tol_outer=delta, step_size=100)['Sigma']
+                            
+        #             # Calculating inverse of Omega_g for Omega_k and w_gk updates
+        #             inv0[g,:,:] = np.linalg.inv(Omega0[g,:,:])
+                    
+         # 2b step: updating weights
+
+         # Weight matrix where each row is for a cluster and each column for a subject
+                    
+        for i in range(G):
+            det0 = np.linalg.det(Omega0[i, :, :])
+            for j in range(K):
+                wgk[i, j] = np.log(pigs[i]) - lambda2 / 2 * np.sum(np.diag(np.dot(inv0[i, :, :],\
+                                Omegas[j,:, :]))) + (-lambda2 / 2) * (np.log(1 / lambda2 ** p) + np.log(det0))
+        wgk = np.apply_along_axis(lambda column: np.exp(column - np.max(column))\
+                                  / np.sum(np.exp(column - np.max(column))), axis=0, arr=wgk)
+            
+            # 3rd step: updating subject-level precision matrices
+        sk = np.array([((nks[k] * Sl[k, :, :] + lambda2 * np.sum([wgk[g, k] * inv0[g, :, :] \
+                        for g in range(G)], axis=(0, 1))) / (nks[k] + lambda2 - p - 1)) for k in range(K)])
+        
+        rhoMat = np.array([np.full((p, p), lambda1 / (nks[x] + lambda2 - p - 1)) for x in range(K)])
+        
+        for k in range(K):
+                    np.fill_diagonal(rhoMat[k,:, :], 0)
+                    Omegas[k, :, :] = sklearn.covariance.graph_lasso(sk[k, :, :], alpha=rhoMat[k, :, :], mode='cd')[1]
+                    Omegas[k, :, :] = (Omegas[k, :, :] + Omegas[k, :, :].T) / 2.0
+        # 4th step: updating weights
+
+         # Weight matrix where each row is for a cluster and each column for a subject
+        for i in range(G):
+                det0 = np.linalg.det(Omega0[i,:, :])
+                for j in range(K):
+                    wgk[i, j] = np.log(pigs[i]) - lambda2 / 2 * np.sum(np.diag(inv0[i, :, :] \
+                                   @ Omegas[j, :, :])) - (lambda2 / 2) * (np.log(1 / lambda2**p) + np.log(det0))          
+                
+        wgk = np.apply_along_axis(lambda column: np.exp(column - np.max(column)), axis=1, arr=wgk)
+        wgk = np.apply_along_axis(lambda column: column / np.sum(column), axis=1, arr=wgk)
+        if np.sum(np.isnan(wgk)) > 0:
+             raise Exception("NaN value detected in wgk")
+         # Record BCD iteration
+         
+        counter += 1
+        deltas = np.concatenate((deltas, [np.max(np.abs(Omega0 - Omega0_old)), np.max(np.abs(Omegas - Omegas_old))]))
+        wArray[counter+1, :, :] = wgk
+
+
+    res = {"Omega0": Omega0, "Omegas": Omegas, "weights": wgk}
+    return res
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
